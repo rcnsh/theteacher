c5c900fbfaaabb04ce367fb86c1bef268c3915a7
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2023-11-03 10:19:24 +0000

    format and make nice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,20 @@ import { type AppType } from "next/dist/shared/lib/utils";
 
 import "@/styles/globals.css";
 
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 
 const interFont = Inter({
-    display: 'swap',
-    subsets: ['latin'],
-    variable: '--font-sans',
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-      <main className={interFont.className}>
+    <main className={interFont.className}>
       <Component {...pageProps} />
-      </main>);
+    </main>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,10 @@ export default function Home() {
           </section>
         </header>
         <main className="grid h-[85dvh] grid-cols-2 gap-4 p-8">
-          <div className="flex flex-col items-center justify-center space-y-2">
+          <Link
+            className="flex flex-col items-center justify-center space-y-2 text-2xl text-gray-800 hover:bg-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
+            href="https://theteacherfiles.rcn.sh/Programming-Development"
+          >
             <svg
               className=" h-32 w-32 text-gray-500 dark:text-gray-400"
               fill="none"
@@ -32,14 +35,12 @@ export default function Home() {
               <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
               <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
             </svg>
-            <Link
-              className="text-2xl text-gray-800 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
-              href="https://theteacherfiles.rcn.sh/Programming-Development"
-            >
-              Programming and Development
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2">
+            Programming and Development
+          </Link>
+          <Link
+            className="flex flex-col items-center justify-center space-y-2 text-2xl text-gray-800 hover:bg-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
+            href="https://theteacherfiles.rcn.sh/Architecture-data-comms-and-applications"
+          >
             <svg
               className=" h-32 w-32 text-gray-500 dark:text-gray-400"
               fill="none"
@@ -55,14 +56,12 @@ export default function Home() {
               <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
               <path d="m15 5 4 4" />
             </svg>
-            <Link
-              className="text-2xl text-gray-800 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
-              href="https://theteacherfiles.rcn.sh/Architecture-data-comms-and-applications"
-            >
-              Architecture
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2">
+            Architecture
+          </Link>
+          <Link
+            className="flex flex-col items-center justify-center space-y-2 text-2xl text-gray-800 hover:bg-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
+            href="https://theteacherfiles.rcn.sh/Programmed-Solution"
+          >
             <svg
               className=" h-32 w-32 text-gray-500 dark:text-gray-400"
               fill="none"
@@ -78,14 +77,12 @@ export default function Home() {
               <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
               <polyline points="14 2 14 8 20 8" />
             </svg>
-            <Link
-              className="text-2xl text-gray-800 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
-              href="https://theteacherfiles.rcn.sh/Programmed-Solution"
-            >
-              Programmed Solution to a Problem
-            </Link>
-          </div>
-          <div className="flex flex-col items-center justify-center space-y-2">
+            Programmed Solution to a Problem
+          </Link>
+          <Link
+            className="flex flex-col items-center justify-center space-y-2 text-2xl text-gray-800 hover:bg-gray-700 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
+            href="https://theteacherfiles.rcn.sh/Database Handbook"
+          >
             <svg
               className=" h-32 w-32 text-gray-500 dark:text-gray-400"
               fill="none"
@@ -103,13 +100,8 @@ export default function Home() {
               <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
               <path d="M16 3.13a4 4 0 0 1 0 7.75" />
             </svg>
-            <Link
-              className="text-2xl text-gray-800 hover:text-blue-500 dark:text-white dark:hover:text-blue-300"
-              href="https://theteacherfiles.rcn.sh/Database Handbook"
-            >
-              Database Handbook
-            </Link>
-          </div>
+            Database Handbook
+          </Link>
         </main>
         <footer className="flex items-center justify-end border-t border-gray-200 bg-white px-6 py-4 dark:border-gray-600 dark:bg-gray-800">
           Made by rcn.sh
